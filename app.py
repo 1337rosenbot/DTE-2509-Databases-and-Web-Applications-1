ffa4496c7b238aa4d6c6b75f5bd86a1bafa24a33
1337rosenbot <mro137@post.uit.no>
2025-03-11 15:19:07 +0100

    Finished lesson

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
+# Common name for files app.y, main.py, __init__.py
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # Create the Flask application
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello, Flask!'
+
+@app.route('/programming')
+def prog():
+    languages = [
+        "Python",
+        "Swiftt",
+        "C++",
+        "C#",
+        "Java",
+        "Kotlin"
+    ]
+    return render_template('prog.html', prog_lang=languages)
+
+
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) # Run the Flask application
